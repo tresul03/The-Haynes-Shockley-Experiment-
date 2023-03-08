c3585809086635835af60fa8cd2b4aebbfc5ef73
blackout-03 <resul13@outlook.com>
2023-03-08 12:35:20 +0000

    Added simulation for diffusion + drift

--- src/diffusion.py
+++ src/diffusion.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FFMpegWriter
+import time
 plt.rcParams["animation.ffmpeg_path"] = "C:\\ffmpeg\\bin\\ffmpeg.exe"
 
 MOBILITY = 1e-1
@@ -22,18 +23,18 @@ plt.ylim(0, 20)
 normaliser = lambda t: (np.sqrt(4*np.pi*D*t)) **-1 #ensures probability of finding charge carrier anywhere in semiconductor = 1
 diffusion_1d = lambda x, t: np.exp(-x**2 / (4*D*t)) #term for modelling diffusion among minority charge carriers in semiconductor
 diffusion_drift_1d = lambda x, t: np.exp(-(x - v*t)**2 / (4*D*t)) #same term as above, but for when an electric field is applied across the semiconductor
-decay = lambda t: carrier_conc*np.exp(-t/tau)
+decay = lambda t: np.exp(-t/tau)
 
 
 metadata = dict(title="Diffusion", artist="Resul Teymuroglu")
 writer = FFMpegWriter(fps=100, metadata=metadata)
 
-xlist = np.linspace(-1, 1, 1000)
-ylist = np.zeros(1000)
+xlist = np.linspace(-1, 1, 10000)
+ylist = np.zeros(10000)
 
 xlabel, ylabel = "Displacement / m", "Charge Carrier Concentration" 
 
-#charge carrier diffusion:
+# charge carrier diffusion:
 with writer.saving(fig, "plots/diffusion.mp4", 100):
     for tval in np.linspace(0, 10, 1000):
         match tval:
@@ -67,15 +68,15 @@ with writer.saving(fig, "plots/diffusion_with_drift.mp4", 100):
         ylist = np.array([])
 print("Sim 2 done.")
 
-
 #charge carrier diffusion + decay:
 with writer.saving(fig, "plots/diffusion_with_decay.mp4", 100):
+    plt.xlim(-0.01, 0.01)
+    plt.ylim(0, 0.5)
     for tval in np.linspace(0, 1e-3, 1000):
         match tval:
             case 0:
                 ylist = xlist * 0
             case _:
-                print(decay(tval))
                 ylist = decay(tval) * normaliser(tval) * diffusion_1d(xlist, tval)
 
         plt.xlabel(xlabel)
@@ -85,8 +86,3 @@ with writer.saving(fig, "plots/diffusion_with_decay.mp4", 100):
         l.set_data(xlist, ylist)
         writer.grab_frame()
         ylist = np.array([])
-print("Sim 3 done.")
-
-#todo: do another simulation accounting for carrier lifetime
-
-#! the animation for the decay equation is extremely narrow and tall, diffusing improperly
---
+++ src/functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(void){
+
+
+
+    return 0;
+}
+
+
+//todo: write the time-dependent normalisation function
+//todo: write the time-dependent decay function
+//todo: write the function for charge carrier population
+//todo: write the random-walk algorithm
