cafc2e2c571af6c2dd1cfc5992b8ddb3171c31e3
blackout-03 <resul13@outlook.com>
2023-03-22 11:24:50 +0000

    Add __init__

--- src/random_walk_v3.py
+++ src/random_walk_v3.py
@@ -5,22 +5,27 @@ from values import Values
 from diffusion_v2 import Diffusion
 
 class RandomWalk():
+    def __init__(self):
+        self.xlabel = "Displacement / m"
+        self.ylabel = "Charge Carrier Concentration / $n_{0}$"
+        self.values = Values()
+
     def random_walk(self, upper: int) -> dict:
         walker = 0 #initial particle position
         positions = [] #list of final positions reached by each particle
 
-        for _ in range(10000):
-            for _ in range(1000):
-                match random.randint(0, 100) > upper: 
-                    case False:
+        for _ in range(10000): #10000 particles
+            for _ in range(1000): #1000 steps per particle
+                match random.randint(0, 100) > upper: #probability of moving left 
+                    case False: 
                         walker -= 1
                     case _:
                         walker += 1
 
-            positions.append(walker)
+            positions.append(walker) #adds final position to list
             walker = 0 #resets position for next particle
 
-        dict_positions = {}
+        dict_positions = {} #dictionary of positions and number of particles at that position
         for i in range(min(positions), max(positions)+1):
             if positions.count(i) > 0:
                 dict_positions[i] = positions.count(i)
@@ -28,7 +33,7 @@ class RandomWalk():
         return dict_positions
 
     def diffusion(self) -> dict:
-        diffusion_1d = lambda x: np.exp(-x**2 / (4*Values.D))
+        diffusion_1d = lambda x: np.exp(-x**2 / (4*self.values.D))
         keys = np.linspace(-1, 1, 1001)
         values = diffusion_1d(keys)
 
@@ -36,9 +41,9 @@ class RandomWalk():
 
 randomer = RandomWalk()
 diffusion = Diffusion()
-plotter = Plotter("Displacement / m", "$P(x, t)$", "random")
-plotter2 = Plotter("Displacement / m", "$P(x, t)$", "random-multiple")
-plotter3 = Plotter("Displacement / m", "$P(x, t)$", "decay_static")
+plotter = Plotter(randomer.xlabel, randomer.ylabel, "random")
+plotter2 = Plotter(randomer.xlabel, randomer.ylabel, "random-multiple")
+plotter3 = Plotter(randomer.xlabel, randomer.ylabel, "decay_static")
 
 xlist = np.linspace(-1, 1, 500000)
 tlist = np.linspace(0.4e-4, 1e-4, 10)
