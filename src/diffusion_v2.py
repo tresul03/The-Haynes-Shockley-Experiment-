24c7961fe11c2c740fc2eb7d2b3baa8f2daf2dec
blackout-03 <resul13@outlook.com>
2023-03-22 11:25:39 +0000

    Initialise Values as values in __init__

--- src/diffusion_v2.py
+++ src/diffusion_v2.py
@@ -7,18 +7,19 @@ class Diffusion:
         self.xlabel = "Displacement / m"
         self.ylabel = "Charge Carrier Concentration / $n_{0}$"
         self.xlist = np.linspace(-1, 1, 50001)
+        self.values = Values()
 
     def normaliser(self, t):
-        return np.sqrt(4*np.pi*Values.D*t) ** -1
+        return np.sqrt(4*np.pi*self.values.D*t) ** -1
 
     def diffusion_1d(self, x, t):
-        return self.normaliser(t) * np.exp(-x**2 / (4*Values.D * t))
+        return self.normaliser(t) * np.exp(-x**2 / (4*self.values.D * t))
 
     def diffusion_drift_1d(self, x, t):
-        return self.normaliser(t) * np.exp(-(x - Values.v*t)**2 / (4*Values.D * t))
+        return self.normaliser(t) * np.exp(-(x - self.values.v*t)**2 / (4*self.values.D * t))
 
     def diffusion_decay_1d(self, x, t):
-        return self.diffusion_drift_1d(x, t) * np.exp(-t/Values.TAU)
+        return self.diffusion_drift_1d(x, t) * np.exp(-t/self.values.TAU)
 
 
 # diffusion = Diffusion()
