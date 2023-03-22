6196dfef536a00465888e5b28b7a7b897ade864b
blackout-03 <resul13@outlook.com>
2023-03-22 12:12:04 +0000

    Change mobility funciton

--- src/dopant_mobility.py
+++ src/dopant_mobility.py
@@ -5,7 +5,7 @@ from plotter import Plotter
 class DopantMobility():
     def __init__(self):
         self.temp_list = np.linspace(10, 1000, 50001)
-        self.mobility = lambda t: 5.5e6 * (t**-1.5)
+        self.mobility = lambda t: 2.4e8 * (t**-2.3)
         
 
 
