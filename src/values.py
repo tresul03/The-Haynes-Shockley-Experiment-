41bf49fab43d41f4afb101da6762f74d846ca37a
blackout-03 <resul13@outlook.com>
2023-03-22 10:25:54 +0000

    Add __init__

--- src/values.py
+++ src/values.py
@@ -1,13 +1,13 @@
 class Values:
-    MOBILITY = 1e-1     #Charge Carrier Mobility
-    BARLENGTH = 950e-6  #Length of Semiconductor
-    VOLTAGE = 6e-2      #Voltage across semiconductor
-    K = 1.38e-23        #Boltzmann's Constant
-    T = 300             #Temperature
-    Q = 1.6e-19         #Electron Charge
-    TAU = 10e-6         #Charge Carrier Lifetime
-    CARRIER_CONC = 1e14 #Charge Carrier Concentration
-
-    v = (MOBILITY * VOLTAGE) / BARLENGTH    #Charge Carrier Drift Velocity
-    D = (MOBILITY*K*T)/Q                    #Diffusion Constant
+    def __init__(self):
+        self.MOBILITY = 1e-1     #Charge Carrier Mobility
+        self.BARLENGTH = 950e-6  #Length of Semiconductor
+        self.VOLTAGE = 6e-2      #Voltage across semiconductor
+        self.K = 1.38e-23        #Boltzmann's Constant
+        self.T = 300             #Temperature
+        self.Q = 1.6e-19         #Electron Charge
+        self.TAU = 10e-6         #Charge Carrier Lifetime
+        self.CARRIER_CONC = 1e14 #Charge Carrier Concentration
+        self.v = (self.MOBILITY * self.VOLTAGE) / self.BARLENGTH    #Charge Carrier Drift Velocity
+        self.D = (self.MOBILITY*self.K*self.T)/self.Q               #Diffusion Constant
 
