dccac221b3038707a56bbd26611696b436f12404
blackout-03 <resul13@outlook.com>
2023-03-22 11:48:09 +0000

    Rename dopant_mobility_v2.py to dopant_mobility.py

---
+++ src/dopant_mobility.py
@@ -0,0 +1,15 @@
+import numpy as np
+from values import Values
+from plotter import Plotter
+
+class DopantMobility():
+    def __init__(self):
+        self.temp_list = np.linspace(10, 1000, 50001)
+        self.mobility = lambda t: 5.5e6 * (t**-1.5)
+        
+
+
+
+plotter = Plotter("Temperature / K", "Mobility / $cm^2 V^{-1} s^{-1}$", "mobility")
+plotter.plot_graph(DopantMobility().temp_list, DopantMobility().mobility(DopantMobility().temp_list), xlims=(150, 400), ylims=(0, 2e3), marker="None", ls="-")
+print(DopantMobility().mobility(300))
