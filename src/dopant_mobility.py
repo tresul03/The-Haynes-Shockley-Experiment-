cf7d79b275062cc91debf5db54cb57a7314badc7
blackout-03 <resul13@outlook.com>
2023-03-22 12:33:11 +0000

    Add labels to __init__

--- src/dopant_mobility.py
+++ src/dopant_mobility.py
@@ -1,15 +1,14 @@
 import numpy as np
-from values import Values
-from plotter import Plotter
 
 class DopantMobility():
     def __init__(self):
+        self.xlabel = "Temperature / K"
+        self.ylabel = "Mobility / $cm^2 V^{-1} s^{-1}$"
+
         self.temp_list = np.linspace(10, 1000, 50001)
         self.mobility = lambda t: 2.4e8 * (t**-2.3)
-        
 
 
-
-plotter = Plotter("Temperature / K", "Mobility / $cm^2 V^{-1} s^{-1}$", "mobility")
-plotter.plot_graph(DopantMobility().temp_list, DopantMobility().mobility(DopantMobility().temp_list), xlims=(150, 400), ylims=(0, 2e3), marker="None", ls="-")
-print(DopantMobility().mobility(300))
+# plotter = Plotter("Temperature / K", "Mobility / $cm^2 V^{-1} s^{-1}$", "mobility")
+# plotter.plot_graph(DopantMobility().temp_list, DopantMobility().mobility(DopantMobility().temp_list), xlims=(150, 400), ylims=(0, 2e3), marker="None", ls="-")
+# print(DopantMobility().mobility(300))
