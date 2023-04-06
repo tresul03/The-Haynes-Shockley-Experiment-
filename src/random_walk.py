e0c8c9581aded1e1cd8b96de9ff77960790a803f
blackout-03 <resul13@outlook.com>
2023-04-06 01:09:37 +0100

    Add decay probability

--- src/random_walk.py
+++ src/random_walk.py
@@ -7,26 +7,29 @@ from diffusion import Diffusion
 class RandomWalk():
     def __init__(self):
         self.xlabel = "Displacement / m"
-        self.ylabel = "Charge Carrier Concentration / $n_{0}$"
+        self.ylabel = "Frequency"
         self.values = Values() #initialises values
         self.xlist = np.linspace(-1, 1, 500000)
-        self.tlist = np.linspace(0.4e-4, 1e-4, 10) 
+        self.tlist = np.linspace(0.4e-4, 1e-4, 15) 
 
-    def random_walk(self, skew_point: int) -> dict: #skew_point is the probability of moving left
+    def random_walk(self, skew_point: int, decay_prob=0) -> dict: #skew_point is the probability of moving right, decay_prob is the probability of decay
         walker = 0 #initial particle position
         positions = [] #list of final positions reached by each particle
 
         for _ in range(10000): #10000 particles
             for _ in range(1000): #1000 steps per particle
-                match random.randint(0, 100) >= skew_point: #probability of moving left 
+                match random.randint(0, 100) <= skew_point: #probability of moving right 
                     case False: 
                         walker -= 1
                     case _:
                         walker += 1
+            
+            if random.randint(0, 100) >= decay_prob:
+                positions.append(walker)
 
-            positions.append(walker) #adds final position to list
             walker = 0 #resets position for next particle
-
+        
+        # print(len(positions))
         dict_positions = {} #dictionary of positions and number of particles at that position
         for i in range(min(positions), max(positions)+1):
             if positions.count(i) > 0:
