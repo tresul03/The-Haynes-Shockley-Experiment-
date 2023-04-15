6f6a342c090661e692b724f8f3808138c497508e
blackout-03 <resul13@outlook.com>
2023-04-15 04:56:32 +0100

    Add plot_colourmaps in main

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ import time
 if __name__ == "__main__":
     start_time = time.time()
 
+    # Please allow up to 5 minutes for the program to run.
     brains = Brains()
+    brains.plot_colourmaps()
     brains.plot_graphs() 
-    brains.produce_videos()
+    brains.plot_videos()
 
     print(f"--- Runtime: {time.time() - start_time} seconds ---")
