1c358f0839a570cb2c3af127d1a536e0b8812526
blackout-03 <resul13@outlook.com>
2023-04-15 02:45:14 +0100

    Add runtime string to measure performance

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from brains import Brains
+import time
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     brains = Brains()
     brains.plot_graphs() 
     brains.produce_videos()
+
+    print(f"--- Runtime: {time.time() - start_time} seconds ---")
