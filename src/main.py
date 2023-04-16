45b35b0c5ccff0f2c2be4b3ccc4a8686e9648192
blackout-03 <resul13@outlook.com>
2023-04-16 23:20:42 +0100

    Change production order in main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # Please allow up to 5 minutes for the program to run.
     brains = Brains()
     start_time = time.time()
-    brains.plot_colourmaps()
-    brains.plot_graphs() 
     brains.plot_videos()
+    brains.plot_graphs()
+    brains.plot_colourmaps()
     print(f"--- Runtime: {time.time() - start_time} seconds ---")
